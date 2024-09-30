d5e46132f2fee230393d024d37fd56b666fde89f
yoshiyaSawadaAh <sawada_yoshiya@accelhack.com>
2024-09-30 10:04:53 +0900

    チャプター12まで

---
+++ app/ui/invoices/button.tsx
@@ -0,0 +1,15 @@
+import { deleteInvoice } from '@/app/lib/actions';
+import { TrashIcon } from '@heroicons/react/24/outline';
+
+export function DeleteInvoice({ id }: { id: string }) {
+  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+ 
+  return (
+    <form action={deleteInvoiceWithId}>
+      <button type="submit" className="rounded-md border p-2 hover:bg-gray-100">
+        <span className="sr-only">Delete</span>
+        <TrashIcon className="w-4" />
+      </button>
+    </form>
+  );
+}
